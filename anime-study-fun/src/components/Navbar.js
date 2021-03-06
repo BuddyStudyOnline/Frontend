77888cd24fc22d42ddf4a446d595929b6cb3deff
VyVy Ngo <vyvy.ngo@mail.utoronto.ca>
2021-03-06 16:27:16 -0500

    Add components

---
+++ anime-study-fun/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+export const NavigationBar = () => (
+    <Navbar collapseOnSelect style={{width: "100%"}} expand="lg" bg="dark" variant="dark" className="main-nav">
+
+    </Navbar>
+)
